df5ff3bb7d78c1f5fe82067bf0c135501a73b9e5
adnmzlz <aidan.may12@gmail.com>
2026-05-08 23:06:03 +1000

    Got it running

---
+++ JS/app.js
@@ -0,0 +1,75 @@
+import { pericles } from "./pericles.js";
+
+let isRunning = true;
+let longestOutput = '';
+let currentString = '';
+let totalCharacters = 0;
+
+const characters = [
+  'A', 'C', 'T', ' ', '1', '\n', '=', 'h',  'o', 'r',
+  'u', 's', '[', 'E', 'n', 't',  'e', 'G',  'w', '.',
+  ']', 'O', 'W', 'R', 'i', 'g',  'a', 'l',  'd', ',',
+  'F', 'm', 'c', "'", 'f', 'y',  'p', 'I',  'b', 'v',
+  'H', 'k', 'q', 'M', ':', 'B',  'S', 'x',  ';', '!',
+  'j', 'P', 'N', 'U', 'Y', 'L',  'z', '\t', 'J', '-',
+  'D', '?', 'K', '2', '(', ')',  '3', '4',  'Z', '"',
+  'Q', '5', 'V', '6'
+];
+
+function getReadableUptime() {
+  const totalSeconds = performance.now() / 1000
+  // const nodeTotalSeconds = process.uptime(); << for server
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = Math.floor(totalSeconds % 60);
+  
+  let hrs = "";
+  if (hours < 10) {
+    hrs = "0" + hours;
+  } else {
+    hrs = hours;
+  }
+
+  let mins = "";
+  if (minutes < 10) {
+    mins = "0" + minutes;
+  } else {
+    mins = minutes;
+  }
+
+  let secs = "";
+  if (seconds < 10) {
+    secs = "0" + seconds;
+  } else {
+  secs = seconds;
+  }
+  
+  return `${hrs}:${mins}:${secs}`;
+}
+
+function run() {
+  if (!isRunning) return;
+
+  document.getElementById("uptime").textContent = getReadableUptime();
+
+  while (pericles.includes(currentString)) {
+    currentString += characters[Math.floor(Math.random() * 74)];
+    totalCharacters++;
+    document.getElementById("totalChars").textContent = totalCharacters;
+
+    if (currentString.length > longestOutput.length) {
+      longestOutput = currentString;
+      document.getElementById("longestText").textContent = longestOutput;
+
+      longestLength = output.length;
+      document.getElementById("longestLength").textContent = longestOutput.length;
+    }
+  }
+
+  document.getElementById("output").textContent += currentString;
+  currentString = '';
+
+  setTimeout(run, Math.max(20, Math.floor(Math.random() * 80)));
+}
+
+run();
---
+++ pericles/acknowledgement.md
@@ -0,0 +1,7 @@
+Pericles, Prince of Tyre
+by William Shakespeare
+Edited by Barbara A. Mowat and Paul Werstine
+  with Michael Poston and Rebecca Niles
+Folger Shakespeare Library
+https://shakespeare.folger.edu/shakespeares-works/pericles/
+Created on Jul 31, 2015, from FDT version 0.9.2
